51f3bca207bd3b482cfde2e70b7daa5b40e089e6
JustinSsu <ssu.pei.je@gmail.com>
2024-10-22 11:15:01 +0800

    add overall layout and change dir

---
+++ frontend/src/app/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import BasicTabs from '../overalllayout/OverallLayout';
+
+function App() {
+    return (
+        <div className="App">
+            <BasicTabs />
+        </div>
+    );
+}
+
+export default App;
