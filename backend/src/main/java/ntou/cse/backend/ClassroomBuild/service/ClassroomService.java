018ec11281a43e521f4ec8e2a693f6578f68bd34
JustinSsu <ssu.pei.je@gmail.com>
2024-10-22 12:01:14 +0800

    add api

--- backend/src/main/java/ntou/cse/backend/ClassroomBuild/service/ClassroomService.java
+++ backend/src/main/java/ntou/cse/backend/ClassroomBuild/service/ClassroomService.java
@@ -6,9 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class ClassroomService {
@@ -16,50 +13,19 @@ public class ClassroomService {
     @Autowired
     private ClassroomRepository classroomRepository;
 
-    private final String[][] classrooms = {
-            {"B1", "B07"}, {"B1", "B14"}, {"B1", "B16"},
-            {"1", "101"}, {"1", "105"}, {"1", "110"}, {"1", "112"}, {"1", "114"},
-            {"2", "201"}, {"2", "203"}, {"2", "205"}, {"2", "212"},
-            {"3", "301"}, {"3", "303"}, {"3", "305"},
-            {"4", "407"}, {"4", "409"}
-    };
-
-    public void initClassrooms() {
-        saveClassroom();
-        removeExtraClassrooms();
+    public List<Classroom> getAllClassrooms() {
+        return classroomRepository.findAll();
     }
 
-    private void saveClassroom() {
-        for (String[] classroomData : classrooms) {
-            String floor = classroomData[0];
-            String roomNumber = classroomData[1];
-
-            if (!classroomExists(floor, roomNumber)) {
-                Classroom classroom = new Classroom();
-                classroom.setFloor(floor);
-                classroom.setRoomNumber(roomNumber);
-                classroomRepository.save(classroom);
-            }
-        }
+    public List<Classroom> getClassroomsByFloor(String floor) {
+        return classroomRepository.findByFloor(floor);
     }
 
-    private boolean classroomExists(String floor, String roomNumber) {
-        Optional<Classroom> existingClassroom = classroomRepository.findByFloorAndRoomNumber(floor, roomNumber);
-        return existingClassroom.isPresent();
+    public Classroom getClassroomByRoomNumber(String roomNumber) {
+        return classroomRepository.findByRoomNumber(roomNumber).orElse(null);
     }
 
-    private void removeExtraClassrooms() {
-        List<Classroom> allClassrooms = classroomRepository.findAll();
-
-        Set<String> predefinedClassroomKeys = List.of(classrooms).stream()
-                .map(classroom -> classroom[0] + classroom[1])
-                .collect(Collectors.toSet());
-
-        for (Classroom classroom : allClassrooms) {
-            String classroomKey = classroom.getFloor() + classroom.getRoomNumber();
-            if (!predefinedClassroomKeys.contains(classroomKey)) {
-                classroomRepository.delete(classroom);
-            }
-        }
+    public List<Classroom> searchClassroomsByKeyword(String keyword) {
+        return classroomRepository.findByFloorContainingOrRoomNumberContaining(keyword, keyword);
     }
 }
