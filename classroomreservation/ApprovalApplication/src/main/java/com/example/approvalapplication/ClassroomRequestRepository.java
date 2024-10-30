2e1fd79f1ce6078ffdf95f329f87ab712c0e849b
JustinSsu <ssu.pei.je@gmail.com>
2024-10-30 21:48:10 +0800

    delete unuse file

--- classroomreservation/ApprovalApplication/src/main/java/com/example/approvalapplication/ClassroomRequestRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.approvalapplication;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.approvalapplication.ClassroomRequest;
-public interface ClassroomRequestRepository extends MongoRepository<ClassroomRequest, String> {
-}
